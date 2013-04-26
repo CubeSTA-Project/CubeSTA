5e37021c98afc33d8155eda948be21eb1becbbcf
Yann Droy <Yann Droy@Yann.home>
2013-04-26 16:27:55 +0200

    traduction polonaise (A FINIR : CARACTERES SPECIAUX) + petites corrections

--- src/edu/cubesta/ressources/L10n.java
+++ src/edu/cubesta/ressources/L10n.java
@@ -158,7 +158,7 @@ public class L10n {
         i18n[4] = "Timer";
         i18n[5] = "Option";
         i18n[6] = "Warning";
-        i18n[7] = "Enter the number of movement for the scramble !";
+        i18n[7] = "Enter the number of movements for the scramble !";
         i18n[8] = "Enter the number of time for the average !";
         i18n[9] = "The number is too small (It should be greater than 3)";
         i18n[10] = "Choose your language !";
@@ -166,7 +166,7 @@ public class L10n {
         i18n[12] = "This is not a number";
         i18n[13] = "Error";
         i18n[14] = "Inspection";
-        i18n[15] = "KeyBoard Help";
+        i18n[15] = "Keyboard Help";
         i18n[16] = "Scramble Help";
         i18n[17] = "Enter the inspection time (in second) !";
         //HELP I18N
@@ -178,12 +178,12 @@ public class L10n {
         i18nHelp[5] = "S : Set the next time as DNS";
         i18nHelp[6] = "R : Reset all times";
         i18nHelp[7] = "F1 : Display the keyboard help";
-        i18nHelp[8] = "F2 : Display the synthax movement help";
+        i18nHelp[8] = "F2 : Display the syntax movement help";
         i18nHelp[9] = "F3 : Set the language of the program";
         i18nHelp[10] = "F9 : Set the inspection time";
-        i18nHelp[11] = "F10 : Set the number of movement for the scramble";
+        i18nHelp[11] = "F10 : Set the number of movements for the scramble";
         i18nHelp[12] = "F11 : Set the number of times to calculate the average";
-        i18nHelp[13] = "F12 : Regenerates a new scramble";
+        i18nHelp[13] = "F12 : Regenerate a new scramble";
         i18nHelp[14] = "F : Front side";
         i18nHelp[15] = "B : Back side";
         i18nHelp[16] = "U : Up side";
@@ -191,7 +191,7 @@ public class L10n {
         i18nHelp[18] = "R : Right side";
         i18nHelp[19] = "L : Left side";
         i18nHelp[20] = "  : Clockwise";
-        i18nHelp[21] = "' : Anticlockwise";
+        i18nHelp[21] = "' : Counterclockwise";
         i18nHelp[22] = "² : Twice";
     }
     
@@ -209,7 +209,7 @@ public class L10n {
         i18n[4] = "Chronom\u00e8tre";
         i18n[5] = "Option";
         i18n[6] = "Attention";
-        i18n[7] = "Entrer le nombre de mouvement pour le m\u00e9lange !";
+        i18n[7] = "Entrer le nombre de mouvements pour le m\u00e9lange !";
         i18n[8] = "Entrer le nombre de temps pour la moyenne !";
         i18n[9] = "Le nombre est trop petit (Il doit \u00eatre sup\u00e9rieur \u00e0 3)";
         i18n[10] = "Choisissez votre langue !";
@@ -219,9 +219,9 @@ public class L10n {
         i18n[14] = "Inspection";
         i18n[15] = "Aide Clavier";
         i18n[16] = "Aide M\u00e9lange";
-        i18n[17] = "Entrer le temps d'inspection (en seconde) !";
+        i18n[17] = "Entrer le temps d'inspection (en secondes) !";
         //HELP I18N
-        i18nHelp[0] = "Echape : Quiter le programme";
+        i18nHelp[0] = "Echap : Quiter le programme";
         i18nHelp[1] = "Espace : D\u00e9marre le chronom\u00e8tre";
         i18nHelp[2] = "Majuscule : D\u00e9marre le temps d'inspection";
         i18nHelp[3] = "Entrer : Ajoute une p\u00e9naliter de 2 secondes";
@@ -229,11 +229,11 @@ public class L10n {
         i18nHelp[5] = "S : D\u00e9finie le temps suivant comme DNS";
         i18nHelp[6] = "R : Remet à z\u00e9ro tous les temps";
         i18nHelp[7] = "F1 : Afficher l'aide clavier";
-        i18nHelp[8] = "F2 : Afficher l'aide de la synthaxe des mouvements";
-        i18nHelp[9] = "F3 : D\u00e9finie la langue du programme";
-        i18nHelp[10] = "F9 : D\u00e9finie le temps d'inspection";
-        i18nHelp[11] = "F10 : D\u00e9finie le nombre de mouvement du m\u00e9lange";
-        i18nHelp[12] = "F11 : D\u00e9finie le nombre de temps pour le calcul de la moyenne";
+        i18nHelp[8] = "F2 : Afficher l'aide de la syntaxe des mouvements";
+        i18nHelp[9] = "F3 : D\u00e9finit la langue du programme";
+        i18nHelp[10] = "F9 : D\u00e9finit le temps d'inspection";
+        i18nHelp[11] = "F10 : D\u00e9finit le nombre de mouvements du m\u00e9lange";
+        i18nHelp[12] = "F11 : D\u00e9finit le nombre de temps pour le calcul de la moyenne";
         i18nHelp[13] = "F12 : Permet de r\u00e9g\u00e9n\u00e9rer un m\u00e9lange";
         i18nHelp[14] = "F : Face avant";
         i18nHelp[15] = "B : Face arri\u00e8re";
@@ -302,7 +302,49 @@ public class L10n {
      */
     
     private static void setPL() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        language = "pl";
+        //MAIN I18N
+        i18n[0] = "Runda";
+        i18n[1] = "Średnia";
+        i18n[2] = "Najlepszy";
+        i18n[3] = "Mieszanie";
+        i18n[4] = "Zegar";
+        i18n[5] = "Ustawienia";
+        i18n[6] = "Uwaga";
+        i18n[7] = "Wpisać liczbę ruchów do pomieszania kostki !";
+        i18n[8] = "Wpisać liczbę czasów do średnii ";
+        i18n[9] = "Liczba jest za mała (powyżej 3)";
+        i18n[10] = "Wybraj język !";
+        i18n[11] = "sekund";
+        i18n[12] = "To nie jest numer";
+        i18n[13] = "Błąd";
+        i18n[14] = "Czas podglądania";
+        i18n[15] = "Pomoc klawiatury";
+        i18n[16] = "Pomoc mieszania";
+        i18n[17] = "Wpisać czas podglądania (w sekundach)!";
+        //HELP I18N
+        i18nHelp[0] = "Echap : Zamknąć program";
+        i18nHelp[1] = "Spacja : Startować zegar";
+        i18nHelp[2] = "Shift : Startować czas podglądania";
+        i18nHelp[3] = "Enter : Dodać karę 2 sekund";
+        i18nHelp[4] = "D : Ustawić poprzedni czasu jako DNF";
+        i18nHelp[5] = "S : Ustawić przyszłego czasu jako DNS";
+        i18nHelp[6] = "R : Resetować zegara";
+        i18nHelp[7] = "F1 : Wyswietlić pomoc klwiatury";
+        i18nHelp[8] = "F2 : Wyswietlić pomoc ruchów kostki";
+        i18nHelp[9] = "F3 : Ustawić język";
+        i18nHelp[10] = "F9 : Ustawić czas podglądania";
+        i18nHelp[11] = "F10 : Ustawić liczbę ruchów do pomieszania kostki";
+        i18nHelp[12] = "F11 : Ustawić liczbę czasów do liczenia średnii";
+        i18nHelp[13] = "F12 : Regenerować jednego mieszania ";
+        i18nHelp[14] = "F : Przednia ściana";
+        i18nHelp[15] = "B : Tylnia ściana";
+        i18nHelp[16] = "U : Górna ściana";
+        i18nHelp[17] = "D : Dolna ściana";
+        i18nHelp[18] = "R : Prawa ściana";
+        i18nHelp[19] = "L : Lewa ściana";
+        i18nHelp[20] = "  : Kierunkiem ruchu według wskazówek zegara";
+        i18nHelp[21] = "' : Kierunkiem przeciwnym według ruchu wskazówek zegara";
+        i18nHelp[22] = "² : Dwa razy";
     }
-    
 }
